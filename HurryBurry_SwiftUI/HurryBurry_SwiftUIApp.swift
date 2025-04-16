6d4bfe27787c1660e4d19c933fa7b4569ce6e8e9
PriyaGnaneshwaran <priyayogi084@gmail.com>
2025-04-16 08:30:49 +0530

    Initial Commit

---
+++ HurryBurry_SwiftUI/HurryBurry_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HurryBurry_SwiftUIApp.swift
+//  HurryBurry_SwiftUI
+//
+//  Created by Priya Gnaneshwaran on 16/04/25.
+//
+
+import SwiftUI
+
+@main
+struct HurryBurry_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
